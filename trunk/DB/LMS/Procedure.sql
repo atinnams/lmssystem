43a515e2565361123b478917464fe501137d8bb6
pthung2902 <pthung2902@48ca209a-22f2-11df-8411-435ed3580d57>
2010-02-27 08:37:36 +0000

    Database for lmssystem.

---
+++ trunk/DB/LMS/Procedure.sql
@@ -0,0 +1,74 @@
+use LMSDB;
+
+if object_id('sp_Add_Point') is not null
+	drop proc sp_Add_Point
+go
+
+create procedure sp_Add_Point(@CardId varchar(16),@TaskID int,@Point int,@MID varchar(15),@TID varchar(8),@PoSCC varchar(2),@Result int output)
+as
+begin	
+	declare @Customer int;
+	begin transaction;	
+	select @Customer = JPOS_IDCustomer from JPOS_Customer where JPOS_CardId = @CardId
+		if  (@@rowcount = 0)
+		begin
+			set @Result = 0;
+			rollback transaction;
+		end
+
+	update JPOS_Customer set JPOS_CurrentPoint = JPOS_CurrentPoint + @Point where JPOS_CardId = @CardId					
+	if  (@@rowcount = 0)
+	begin
+		set @Result = 0;
+		rollback transaction;
+	end
+	
+	insert into JPOS_Log(JPOS_Date,JPOS_Task,JPOS_Customer,JPOS_PointGain,JPOS_PointLoss,JPOS_MID,JPOS_TID,JPOS_PoSCC_ID) 
+	values (getdate(),@TaskID,@Customer,@Point,0,@MID,@TID,@PoSCC)				
+	if  (@@rowcount = 0)
+	begin
+		set @Result = 0;
+		rollback transaction;
+	end
+
+	set @Result = 1;
+	commit transaction;
+end
+go
+
+if object_id('sp_Sub_Point') is not null
+	drop proc sp_Sub_Point
+go
+
+create procedure sp_Sub_Point(@CardId varchar(16),@TaskID int,@Point int,@MID varchar(15),@TID varchar(8),@PoSCC varchar(2),@Result int output)
+as
+begin	
+	declare @Customer int;
+	begin transaction;	
+	select @Customer = JPOS_IDCustomer from JPOS_Customer where JPOS_CardId = @CardId
+		if  (@@rowcount = 0)
+		begin
+			set @Result = 0;
+			rollback transaction;
+		end
+
+	update JPOS_Customer set JPOS_CurrentPoint = JPOS_CurrentPoint - @Point where JPOS_CardId = @CardId
+	if  (@@rowcount = 0)
+	begin
+		set @Result = 0;
+		rollback transaction;
+	end
+	
+	insert into JPOS_Log(JPOS_Date,JPOS_Task,JPOS_Customer,JPOS_PointGain,JPOS_PointLoss,JPOS_MID,JPOS_TID,JPOS_PoSCC_ID) 
+	values (getdate(),@TaskID,@Customer,0,@Point,@MID,@TID,@PoSCC)				
+	if  (@@rowcount = 0)
+	begin
+		set @Result = 0;
+		rollback transaction;
+	end
+
+	set @Result = 1;
+	commit transaction;
+end
+go
+
---
+++ trunk/DB/LMS/function.sql
@@ -0,0 +1,97 @@
+use LMSDB
+
+if object_id('fn_check_card') is not null
+	drop function fn_check_card
+go
+
+create function  fn_check_card(@cardid varchar(16))
+returns int
+as	
+begin	
+	return (select count(*) from JPOS_Card where JPOS_CardId = @cardid);
+end
+go
+
+--test fn_check_card
+/*
+declare @result int;
+select @result = dbo.fn_check_card('1234567887654321');
+select @result; // expected to 1
+
+select @result = dbo.fn_check_card('1234567887654320');
+select @result; // expected to 0
+
+*/
+
+--===========================================================================
+
+if object_id('fn_check_card_expire') is not null
+	drop function fn_check_card_expire
+go
+
+create function  fn_check_card_expire(@cardid varchar(16))
+returns int
+as	
+begin	
+	return (select count(*) from JPOS_Card where JPOS_CardId = @cardid and getdate() > JPOS_ExpireDay);
+end
+go
+
+--test
+/*
+declare @result int;
+select @result = dbo.fn_check_card_expire('1111111111111111');
+select @result ; --expected to 1
+
+declare @result int;
+select @result = dbo.fn_check_card_expire('1234567812345678');
+select @result ; --expected to 0
+
+*/
+--===========================================================================
+
+if object_id('fn_check_merchant') is not null
+	drop function fn_check_merchant
+go
+
+create function  fn_check_merchant(@MID varchar(15),@TID varchar(8))
+returns int
+as	
+begin	
+	return (select count(*) from JPOS_Merchant where JPOS_MID = @MID and JPOS_TID = @TID );
+end
+go
+
+--test fn_check_merchant
+/*
+declare @result int;
+select @result = dbo.fn_check_merchant('000000000000001','00000001');
+select @result; //expected to 1;
+
+select @result = dbo.fn_check_merchant('000000000000001','00000000');
+select @result; //expected to 0;
+*/
+--===========================================================================
+
+if object_id('fn_check_poscc') is not null
+	drop function fn_check_poscc
+go
+
+create function  fn_check_poscc(@PoSCCID varchar(2))
+returns int
+as	
+begin	
+	return (select count(*) from JPOS_PoSCC where JPOS_PoSCC_ID = @PoSCCID) ;
+end
+go
+
+--test
+/*
+declare @result int;
+select @result = dbo.fn_check_poscc('01');
+select @result; --expected to 1;
+
+declare @result int;
+select @result = dbo.fn_check_poscc('04');
+select @result; --expected to 0;
+*/
