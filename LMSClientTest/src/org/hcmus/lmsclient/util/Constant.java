f7b3a995755461aac970d99c3881150bdcc7822f
pthung2902 <pthung2902@48ca209a-22f2-11df-8411-435ed3580d57>
2010-03-26 16:39:26 +0000

    JUnit Client Test.

---
+++ LMSClientTest/src/org/hcmus/lmsclient/util/Constant.java
@@ -0,0 +1,21 @@
+package org.hcmus.lmsclient.util;
+
+public class Constant {
+	
+	//constant message for key context
+	public final static String REQUEST = "REQUEST";
+	public final static String SOURCE = "SOURCE";
+	public final static String QUEUE = "TXNMGRTEST";
+	public final static String RC = "RESPONSECODE";
+	public final static String CONN = "CONNECTION";
+	public final static String POINT = "POINT";
+	
+	//Message which send back to client
+	public final static String MID_OR_TID_NOT_FOUND = "MID OR TID NOT FOUND";
+	public final static String CARD_NOT_FOUND = "INVALID CARD NUMBER";
+	public final static String POSCC_NOT_FOUND = "POSCC NOT FOUND";
+	public final static String OTHER_ERROR = "OTHER ERROR";
+	public final static String EXPIRE_CARD = "EXPIRE CARD";
+	public final static String SUCCESFULL = "TRANSACTION SUCESSFULLY";
+	public final static String INVALID_FIELD = "INVALID FIELD";
+}
