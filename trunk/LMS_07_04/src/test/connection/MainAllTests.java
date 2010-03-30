da5568ddb608e8eeca1ea77ce11688b022e49169
pthung2902 <pthung2902@48ca209a-22f2-11df-8411-435ed3580d57>
2010-03-30 10:44:03 +0000

    change name to use with jpos test.

---
+++ trunk/LMS_07_04/src/test/connection/MainAllTests.java
@@ -0,0 +1,38 @@
+package test.connection;
+
+import test.bus.JPOS_CardBUS_TEST;
+import test.bus.JPOS_CustomerBUS_TEST;
+import test.bus.JPOS_MerchantBUS_TEST;
+import test.bus.JPOS_PoSCCBUS_TEST;
+import test.util.MessageHelper_TEST;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class MainAllTests extends TestCase {
+
+	public MainAllTests(String name) {
+		super(name);
+	}
+	
+	public static Test suite(){
+		TestSuite mainTest = new TestSuite();
+		
+		//Add all test method of JPOS_CardBUS_Test
+		mainTest.addTestSuite(JPOS_CardBUS_TEST.class);
+		
+		//Add all test method of JPOS_CustomerBUS_Test
+		mainTest.addTestSuite(JPOS_CustomerBUS_TEST.class);
+		
+		//Add all test method of JPOS_MerchantBUS_Test
+		mainTest.addTestSuite(JPOS_MerchantBUS_TEST.class);
+		
+		//Add all test method of JPOS_PoSCCBUS_Test
+		mainTest.addTestSuite(JPOS_PoSCCBUS_TEST.class);
+		
+		//Add all test method of MessageHelper_Test
+		mainTest.addTestSuite(MessageHelper_TEST.class);
+		return mainTest;
+	}
+
+}
