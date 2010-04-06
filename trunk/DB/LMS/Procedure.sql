c3e566cab3869d6d464918b6b4944edd1ae941e0
pthung2902 <pthung2902@48ca209a-22f2-11df-8411-435ed3580d57>
2010-04-06 14:40:11 +0000

    Add redemption business

--- trunk/DB/LMS/Procedure.sql
+++ trunk/DB/LMS/Procedure.sql
@@ -36,6 +36,8 @@ begin
 end
 go
 
+-------------------------------------------------------------------------------------------------------------------------------
+
 if object_id('sp_Sub_Point') is not null
 	drop proc sp_Sub_Point
 go
@@ -72,6 +74,7 @@ begin
 end
 go
 
+-------------------------------------------------------------------------------------------------------------------------------
 
 if object_id('sp_card_activate') is not null
 	drop proc sp_card_activate
@@ -83,13 +86,51 @@ begin
 	update JPOS_Card set JPOS_IsActivate = '1' where JPOS_CardId = @cardid;
 end
 
-/*--test
+-------------------------------------------------------------------------------------------------------------------------------
+if object_id('sp_redemption') is not null
+	drop proc sp_redemption
+go
 
-exec sp_card_activate '8765432112345678';
-select * from JPOS_card;
-update JPOS_Card set JPOS_IsActivate = '0' where JPOS_CardId = '8765432112345678';
-
-*/
-
+create proc sp_redemption(@CardId varchar(16),@TaskID int,@giftType int,@MID varchar(15),@TID varchar(8),@PoSCC varchar(2),@Result int output)
+as
+begin
+	declare @Customer int;
+	declare @giftPoint int;
+	declare @id_log int;
+	begin transaction;	
+	select @Customer = JPOS_IDCustomer from JPOS_Customer where JPOS_CardId = @CardId
+		if  (@@rowcount = 0)
+		begin
+			set @Result = 0;
+			rollback transaction;
+		end
+	
+	set @giftPoint = dbo.fn_get_gift_point(@giftType);
+	update JPOS_Customer set JPOS_CurrentPoint = JPOS_CurrentPoint - @giftPoint where JPOS_CardId = @CardId
+	if  (@@rowcount = 0)
+	begin
+		set @Result = 0;
+		rollback transaction;
+	end
+	
+	insert into JPOS_Log(JPOS_Date,JPOS_Task,JPOS_Customer,JPOS_PointGain,JPOS_PointLoss,JPOS_MID,JPOS_TID,JPOS_PoSCC_ID) 
+	values (getdate(),@TaskID,@Customer,0,@giftPoint,@MID,@TID,@PoSCC)				
+	if  (@@rowcount = 0)
+	begin
+		set @Result = 0;
+		rollback transaction;
+	end
+	
+	set @id_log = @@identity
+	insert into JPOS_Log_Exchange(JPOS_IDLog,JPOS_Gift) values(@id_log,@giftType)
+	if  (@@rowcount = 0)
+	begin
+		set @Result = 0;
+		rollback transaction;
+	end
+	
+	set @Result = 1;
+	commit transaction;
+end
 
 
